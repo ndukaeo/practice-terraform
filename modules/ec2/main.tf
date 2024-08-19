40a85890041200eac5f6646a643dbd8fc20046a6
nduka ojeh <nojeh@baneollc.com>
2024-08-18 21:21:46 -0400

    terraform practice

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,9 +3,7 @@ resource "aws_instance" "practice" {
   ami           = var.ami
   instance_type = var.instance_type
   instance_market_options {
-    spot_options {
-      max_price = 0.0031
-    }
+    market_type = "spot"
   }
 
   tags = {
