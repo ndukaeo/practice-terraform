a1c7744229b8de743c1b94ce1692936d62f70b65
nduka ojeh <nojeh@baneollc.com>
2024-08-18 22:20:35 -0400

    terraform practice

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ module "ec2" {
 
   for_each = var.name
 #   count = length(var.name)
-  name          = var.name
-  ami           = var.ami
-  instance_type = var.instance_type
+  name          = each.key
+  ami           = each.value["ami"]
+  instance_type = each.value["instance_type"]
 
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,21 @@
-variable "ami" {
-  default = "ami-041e2ea9402c46c32"
-}
-variable "instance_type" {
-  default = "t3.small"
-}
+# variable "ami" {
+#   default = "ami-041e2ea9402c46c32"
+# }
+# variable "instance_type" {
+#   default = "t3.small"
+# }
+
 variable "name" {
-  default = ["practice-web-1", "practice-web-2"]
+  default = {
+    "practice-web-1" = {
+      ami           = "ami-041e2ea9402c46c32"
+      instance_type = "t3.small"
+    }
+
+
+    "practice-web-2" = {
+      ami           = "ami-041e2ea9402c46c32"
+      instance_type = "t2.micro"
+    }
+  }
 }
