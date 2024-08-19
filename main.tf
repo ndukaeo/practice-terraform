f1f87e31f2d3adb62ae3a7a4173eeb8ac377280a
nduka ojeh <nojeh@baneollc.com>
2024-08-18 22:03:56 -0400

    terraform practice

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 module "ec2" {
   source = "./modules/ec2"
 
-  count = length(var.name)
+  for_each = var.name
+#   count = length(var.name)
   name          = var.name
   ami           = var.ami
   instance_type = var.instance_type
